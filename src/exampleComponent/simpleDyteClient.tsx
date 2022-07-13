ac373ad31bbfa06666f5c7989beab6703d72a2ce
kushagra12 <kushagra@dyte.in>
2022-07-13 13:00:47 +0530

    initial (bad) implementation

--- src/exampleComponent/simpleDyteClient.tsx
+++ src/exampleComponent/simpleDyteClient.tsx
@@ -3,6 +3,17 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useDyteClient, DyteProvider } from "@dytesdk/react-web-core";
 import { DyteMeeting } from "@dytesdk/react-ui-kit";
 import { joinExistingRoom } from "../utils";
+import { SelfieSegmentation } from "@mediapipe/selfie_segmentation";
+
+const selfieSegmentation = new SelfieSegmentation({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/selfie_segmentation/${file}`;
+}});
+
+const background = "";
+
+selfieSegmentation.setOptions({
+  modelSelection: 1,
+});
 
 export const SimpleDyteClient: React.FC<{}> = () => {
   const navigate = useNavigate();
@@ -32,6 +43,36 @@ export const SimpleDyteClient: React.FC<{}> = () => {
         sessionStorage.clear();
         navigate("/");
       });
+
+      let prevResults: any;
+
+      selfieSegmentation.onResults(results => {
+        console.log(results);
+        prevResults = results;
+      });
+
+      selfieSegmentation.initialize().then(() => {
+        function RetroTheme() {
+          console.log('Initialising RetroTheme');
+          return async (canvas: HTMLCanvasElement, canvasCtx: any) => {
+              selfieSegmentation.send({image: canvas});
+              if(prevResults !== undefined) {
+
+                // canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+                canvasCtx.fillStyle = '#00FF007F';
+
+                canvasCtx.drawImage(
+                    prevResults.segmentationMask, 0, 0, canvas.width,
+                    canvas.height);
+
+              }
+  
+          }
+          
+        }
+        // Add video middleware
+        meeting.self.addVideoMiddleware(RetroTheme)
+      })
     }
   }, [meeting, navigate]);
 
